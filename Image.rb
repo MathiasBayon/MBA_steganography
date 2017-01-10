c798d98363de47b4aafec92bb48e23d1e9f089a9
Mathias Bayon <mathias.bayon@gmail.com>
2017-01-10 18:32:18 +0100

    Added comments
    Because I’m a nice guy!

--- Image.rb
+++ Image.rb
@@ -13,8 +13,13 @@ class Messages
 
 end
 
+# Open class extension for String
 class String
 
+  # Add ending flag to string
+  # Ending flag is read from properties file
+  # When adding ending flag, make sure is is not already contained within self string
+  # If so, remove it
   def add_ending_flag
     Messages::get
     ending_flag = Messages::get["cypher"]["ending_flag"]
@@ -23,20 +28,27 @@ class String
 
 end
 
+# Image class : initialize, cypher and decypher messages within PNG image
 class Image
 
     attr_reader :height, :width, :filename, :pixels
 
+    # Initialize Image from filename
     def initialize(filename)
         raise ArgumentError, "File #{filename} does not exist" unless File.exists?(filename)
+        
         Trace::get_logger.info('Image.initialize') { "Initializating Image from file #{filename}..." }
+        
+        # Load image contents
         image_contents = ChunkyPNG::Image.from_file(filename)
 
+        # Set attributes from image content, for easier retrieving
         @height         = image_contents.dimension.height
         @width          = image_contents.dimension.width
         @filename       = filename
         @pixels         = {}
 
+        # Fetch each pixel within file, and initialze pixels matrix
         @width.times do |w|
             @height.times do |h|
                 @pixels[[w,h]] = Pixel.new(
@@ -48,17 +60,30 @@ class Image
         end
     end
 
+    # Cypher provided message within self
     def cypher(message)
         Trace::get_logger.info('Image.cypher') { "Cyphering message..." }
+        
+        # Add ending flag to provided message
         message_w_ending_flag = message.add_ending_flag
+
+        # Check if pixel matric is large enough to store provided message
         raise ArgumentError, "Input file is not big enough to store message" unless is_big_enough_to_store_message?(message_w_ending_flag)
+        
+        # Transform message into binary array, segmented in 3 bits subarrays
         x = y = 0
         message_w_ending_flag.split("").map{|char| char.ord.to_s(2).rjust(8,"0").scan(/.{1,3}/)}
 
+        # Fetch 3 bits subarrays, and store their content in the least significant bit of each pixel color component
         Image::get_message_as_binary_array_with_leading_zeros(message_w_ending_flag).each do |chunk|
+            # Empty least significant bit of pixel color component
             @pixels[[x,y]].reset
+
+            # Store chunk
             @pixels[[x,y]].store(chunk)
             x += 1
+
+            # Climb a row when reaching last pixel column
             if (x == @width)
                 x=0
                 y+=1
@@ -68,11 +93,14 @@ class Image
         self
     end
 
+    # Decypher message enclosed within self
     def decypher
         Trace::get_logger.info('Image.cypher') { "Decyphering message..." }
         
         res_b = res_s = ""
 
+        # Fetch each pixel within image, retrieve least significant bit for its color component
+        # Store result sequentially in res_b string
         for y in (0...@height)
             for x in (0...@width)
                 res_b += @pixels[[x,y]].r.to_s(2).rjust(8, "0")[7].to_s
@@ -81,32 +109,41 @@ class Image
             end
         end
 
+        # Then, fetch octet by octet, convert to char, and append result in res_s string
         for i in (0...res_b.length).step(8)
             chunk = res_b[i...i+8].to_i(2).chr
             break if chunk == Messages::get["cypher"]["ending_flag"]
             res_s += chunk
         end
         
+        # Return decyphered string !
         res_s
     end
 
+    # Write self in output file
     def write(output_filename)
         Trace::get_logger.info('Image.write') { "Writing output file..." }
+        
         output_file = ChunkyPNG::Image.new(@width, @height, ChunkyPNG::Color::TRANSPARENT)
+        
         @width.times do |x|
             @height.times do |y|
                 output_file[x,y] = @pixels[[x,y]].get_color
             end
         end
+        
         output_file.save("#{output_filename}.png", :interlace => false)
     end
 
+    # Return string representation of self
     def to_s
         self.pixels.each { |pixel| pixel.to_s }
     end
 
+    # Get random image with the dimensions provided as parameters
     def self.get_random_image(image_width, image_height)
         png = ChunkyPNG::Image.new(image_width, image_height, ChunkyPNG::Color::TRANSPARENT)
+        
         for x in (0...image_height)
             for y in (0...image_height)
                 png[x,y] = ChunkyPNG::Color.rgba(Random.new.rand(0..255), Random.new.rand(0..255), Random.new.rand(0..255), 255)
@@ -118,10 +155,13 @@ class Image
 
     private
 
+    # Transform string message into binary array, each character beeing represented on 1 octet
+    # Then return 3 bit chunks from it
     def self.get_message_as_binary_array_with_leading_zeros(message)
         message.split("").map{|char| char.ord.to_s(2).rjust(8,"0")}.join.scan(/.{1,3}/)
     end
 
+    # Returns true if self is large enough to store provided message
     def is_big_enough_to_store_message?(message)
         msg_a = Image::get_message_as_binary_array_with_leading_zeros(message)
         msg_a.length <= @width*@height
--- Pixel.rb
+++ Pixel.rb
@@ -1,9 +1,11 @@
 require 'chunky_png'
 
+# Pixel class : represent pixels as 3 RGB color components
 class Pixel
 
     attr_accessor :r, :g, :b, :original_r, :original_g, :original_b
 
+    # Initialize self from RGB int values
     def initialize(r, g, b)
         raise TypeError, "<r>, <g> and <b> must be positive integers" unless ((r.is_a? Numeric) && (g.is_a? Numeric) && (b.is_a? Numeric))
         raise ArgumentError, "<r>, <g> and <b> must be positive integers" unless r >=0 && r <= 255 && g >=0 && g <= 255 && b >=0 && b<= 255
@@ -12,14 +14,17 @@ class Pixel
         @b = @original_b = b
     end
 
+    # Return self as ChunkyPNG::Color
     def get_color
         ChunkyPNG::Color::rgb(@r, @g, @b)
     end
 
+    # Return Array reprenzentation of self
     def to_a
         [r, g, b]
     end
 
+    # Return String reprezentation of self
     def to_s
         self.to_a.to_s
     end
@@ -32,6 +37,7 @@ class Pixel
         self.eql?(pixel)
     end
 
+    # Store 3 bit chunk within self
     def store(three_bits_as_string)
         self.compress_all
         @r += 1 if three_bits_as_string[0] == "1"
@@ -40,6 +46,7 @@ class Pixel
         self
     end
 
+    # Reset RGB color values to original ones
     def reset
         @r = @original_r
         @g = @original_g
@@ -48,12 +55,15 @@ class Pixel
 
     protected
 
+    # Compress every RGB component of self
     def compress_all
         @r = self.compress(@r)
         @g = self.compress(@g)
         @b = self.compress(@b)
     end
 
+    # Get compressed value from provided color_value
+    # "Compressed" meaning : with least significant bit set to 0
     def compress(color_value)
         color_as_octet_array = color_value.to_s(2).rjust(8, "0").split("")
         color_as_octet_array[7] = "0"
